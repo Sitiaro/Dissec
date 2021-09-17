09d0bee93b6aeea339b89610b201ed9c0b05a097
Sitiaro <46511306+Sitiaro@users.noreply.github.com>
2021-09-17 20:47:49 +0300

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,7 @@
+import os
+os.system('pip install bs4')
+os.system('pip install urllib3')
+os.system('pip install lxml')
+os.system('pip install gc')
+os.system('pip install certifi')
+os.system('pip install requests')
